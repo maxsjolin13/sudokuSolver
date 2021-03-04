070d878ff7fb7f4323d518269cffcef560605cc4
Sabewey <44124418+Sabewey@users.noreply.github.com>
2021-03-04 11:30:40 +0100

    Added test cases for JUnit

--- sudoku/src/test/SudokuTests.java
+++ sudoku/src/test/SudokuTests.java
@@ -9,28 +9,55 @@ import org.junit.jupiter.api.Test;
 
 class SudokuTests {
 	sudoku s1;
-	sudoku s2;
-	sudoku s3;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		this.s1 = new sudoku();
-		this.s2 = new sudoku();
-		this.s3 = new sudoku();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		this.s1 = null;
-		this.s2 = null;
-		this.s3 = null;
+	}
+	
+	/*
+	 * Gets empty, sets number, and clears cell.
+	 */
+	@Test
+	void getSetClear() {
+		assertEquals(0, s1.getNumber(0, 0));
+		s1.setNumber(0, 0, 9);
+		assertEquals(9, s1.getNumber(0, 0));
+		s1.clearNumber(0, 0);
+		assertEquals(0, 0);
+	}
+	
+	/*
+	 * Sets entire matrix to 9, then clear, then checks each cell individually.
+	 */
+	@Test
+	void clearAll() {
+		int[][] a = new int[s1.getDimension()][s1.getDimension()];
+		for(int i = 0; i < s1.getDimension(); i ++) {
+			for (int j = 0; j < s1.getDimension(); j++) {
+				a[i][j] = 9;
+			}
+		}
+		s1.setMatrix(a);
+		
+		s1.clear();
+		for(int r = 0; r < s1.getDimension(); r ++) {
+			for (int c = 0; c < s1.getDimension(); c++) {
+				assertEquals(0, s1.getNumber(r, c));
+			}
+		}
 	}
 
 	/*
-	 * Fills s1 with empty board
+	 * Fills s1 with empty board, then solves it.
 	 */
 	@Test
-	void allZerosSolve() {
+	void SolveAllZeros() {
 		int[][] a = new int[s1.getDimension()][s1.getDimension()];
 		for(int i = 0; i < s1.getDimension(); i ++) {
 			for (int j = 0; j < s1.getDimension(); j++) {
@@ -40,6 +67,46 @@ class SudokuTests {
 		s1.setMatrix(a);
 		
 		assertEquals(true, s1.solve(), "Cannot solve empty board");
+	}
+	
+	/*
+	 * Creates unsolvable sudoku with duplicate number in first subgrid.
+	 */
+	@Test
+	void SolveUnsolvable() {
+		int[][] a = new int[s1.getDimension()][s1.getDimension()];
+		for(int i = 0; i < s1.getDimension(); i ++) {
+			for (int j = 0; j < s1.getDimension(); j++) {
+				a[i][j] = 0;
+			}
+		}
+		s1.setMatrix(a);
+		s1.setNumber(0, 0, 9);
+		s1.setNumber(2, 2, 9);
+		
+		assertEquals(false, s1.solve(), "Solved the unsolvable?!");
+	}
+	
+	/*
+	 * Fills grid with sudoku from fig.1 and proceeds to solve.
+	 */
+	@Test
+	void SolveFig1() {
+		int[][] b = {
+				{0, 0, 8, 0, 0, 9, 0, 6, 2},
+			    {0, 0, 0, 0, 0, 0, 0, 0, 5},
+			    {1, 0, 2, 5, 0, 0, 0, 0, 0},
+			    {0, 0, 0, 2, 1, 0, 0, 9, 0},
+			    {0, 5, 0, 0, 0, 0, 6, 0, 0},
+			    {6, 0, 0, 0, 0, 0, 0, 2, 8},
+			    {4, 1, 0, 6, 0, 8, 0, 0, 0},
+			    {8, 6, 0, 0, 3, 0, 1, 0, 0},
+			    {0, 0, 0, 0, 0, 0, 4, 0, 0},
+			    };
+		
+		s1.setMatrix(b);
+		
+		assertEquals(true, s1.solve(), "Cannot solve fig.1 sudoku");
 	}
 
 }
